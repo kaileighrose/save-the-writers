c6745144ada5c3a9c7632f6509509d94a2a9124f
kaileighrose <kaileighrose@gmail.com>
2016-06-24 13:43:21 -0400

    fix shotgun issue, add helper methods, build out sign up and login processes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require './config/environment'
-require "./app/models/user"
+require './app/models/user'
 
 class ApplicationController < Sinatra::Base
 
@@ -19,8 +19,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
+    if !logged
     erb :'users/create_user'
   end
 
+  post '/signup' do
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+        redirect "/login"
+    else
+        redirect "/failure"
+    end
+  end
 
+  get 'resources/:id' do
+    @resource = Resource.find_by_id(params[:id])
+    erb :'resources/show_resource'
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to "/tweets"
+    else
+        redirect to "/failure"
+    end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
